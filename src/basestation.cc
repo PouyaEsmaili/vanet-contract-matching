c90ae4fcadbbffc422509bc77adb7f1d196673f0
Pouya <poesmaili@gmail.com>
2024-01-09 14:29:07 +0330

    Implement optimizer call and contract choice and fix bugs

--- src/basestation.cc
+++ src/basestation.cc
@@ -5,19 +5,30 @@
 
 using namespace omnetpp;
 
-struct Vehicle
-{
+struct Vehicle {
     double taskResource;
     double taskDataSize;
     double delayConstraint;
+    double taskPrice;
     double sharedResource;
     double price;
     bool isTaskReady = false;
-    double totalTime[];
+    bool isTaskAssigned = false;
+    double *totalTime;
 };
 
-class BaseStation : public cSimpleModule
-{
+static size_t writeCallback(void *contents, size_t size, size_t nmemb, std::string *s) {
+    size_t newLength = size * nmemb;
+    try {
+        s->append((char *) contents, newLength);
+    } catch (std::bad_alloc &e) {
+        // handle memory problem
+        return 0;
+    }
+    return newLength;
+}
+
+class BaseStation : public cSimpleModule {
 private:
     // Contrct
     double unitBenefit;
@@ -33,9 +44,10 @@ private:
     int numVehicles;
     Vehicle *vehicles;
 
+    ContractList *contractList;
+
 protected:
-    virtual void initialize() override
-    {
+    virtual void initialize() override {
         // Read parameters from .ned file
         unitBenefit = par("unitBenefit");
         computationCapability = par("computationCapability");
@@ -44,83 +56,71 @@ protected:
         deltaMin = par("deltaMin");
         deltaMax = par("deltaMax");
 
-        cStringTokenizer tokenizer(par("typeProbability"));
-        while (tokenizer.hasMoreTokens())
-        {
+        cStringTokenizer tokenizer(par("typeProbability"), ",");
+        while (tokenizer.hasMoreTokens()) {
             typeProbability.push_back(std::stod(tokenizer.nextToken()));
         }
 
         taskAssignmentThreshold = par("taskAssignmentThreshold");
 
-        numVehicles = gate("vehicles")->size();
+        numVehicles = gateSize("vehicles");
         vehicles = new Vehicle[numVehicles];
 
         cMessage *prepContractsMsg = new cMessage("prepareContracts");
         scheduleAt(0, prepContractsMsg);
     }
 
-    virtual void handleMessage(cMessage *msg) override
-    {
-        if (msg->isSelfMessage())
-        {
+    virtual void handleMessage(cMessage *msg) override {
+        if (msg->isSelfMessage()) {
             // Check if this is the 'prepareContracts' self-message
-            if (strcmp(msg->getName(), "prepareContracts") == 0)
-            {
+            if (strcmp(msg->getName(), "prepareContracts") == 0) {
                 // Call the function to prepare contracts
                 prepareContracts();
             }
         }
 
-        // check if cMessage is of type ContractList
-        else if (msg->isName("TaskMetadata"))
-        {
-            handleُTaskMetadata(msg);
-        }
-        else if (msg->isName("ContractChoice"))
-        {
-            ContractChoice *choice = check_and_cast<ContractChoice *>(msg);
-            EV << "Contract choice: " << choice->getType() << "from vehicle: " << msg->getArrivalGate()->getIndex() << endl;
+        else if (msg->isName("handleTaskMetadata")) {
+            handleTaskMetadata(msg);
+        } else if (msg->isName("chooseContract")) {
+            chooseContract(msg);
         }
 
         delete msg;
     }
 
-    void prepareContracts()
-    {
+    void prepareContracts() {
         CURL *curl;
         CURLcode res;
         std::string readBuffer;
 
+        struct curl_slist *headers = nullptr;
+        headers = curl_slist_append(headers, "Content-Type: application/json"); // Add Content-Type header
         // Construct JSON payload
         nlohmann::json data = {
-            {"unit_benefit", unitBenefit},
-            {"computation_capability", computationCapability},
-            {"duration", duration},
-            {"type_probability", typeProbability},
-            {"total_vehicles", totalVehicles},
-            {"delta_min", deltaMin},
-            {"delta_max", deltaMax}};
+                {"unit_benefit",           unitBenefit},
+                {"computation_capability", computationCapability},
+                {"duration",               duration},
+                {"type_probability",       typeProbability},
+                {"total_vehicles",         totalVehicles},
+                {"delta_min",              deltaMin},
+                {"delta_max",              deltaMax}};
         std::string jsonData = data.dump();
 
         curl = curl_easy_init();
-        if (curl)
-        {
+        if (curl) {
             curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:9090");
             curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonData.c_str());
             curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, jsonData.size());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
             curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
-            // Perform the request, res will get the return code
             res = curl_easy_perform(curl);
 
-            if (res == CURLE_OK)
-            {
-                // Parse the JSON response
+            if (res == CURLE_OK) {
                 nlohmann::json responseJson = nlohmann::json::parse(readBuffer);
                 sendContractListToVehicles(responseJson);
-            }
-            else
-            {
+            } else {
                 EV << "curl_easy_perform() failed: " << curl_easy_strerror(res);
             }
 
@@ -128,17 +128,15 @@ protected:
         }
     }
 
-    void sendContractListToVehicles(const nlohmann::json &responseJson)
-    {
+    void sendContractListToVehicles(const nlohmann::json &responseJson) {
         auto deltas = responseJson["delta"];
         auto pies = responseJson["pie"];
 
         // Create a new ContractList message
-        ContractList *contractList = new ContractList("contractList");
+        contractList = new ContractList("processContractList");
         contractList->setContractsArraySize(deltas.size());
 
-        for (size_t i = 0; i < deltas.size(); ++i)
-        {
+        for (size_t i = 0; i < deltas.size(); ++i) {
             Contract contract;
             contract.setResource(deltas[i]);
             contract.setReward(pies[i]);
@@ -146,85 +144,96 @@ protected:
         }
 
         // Send the ContractList message to all connected vehicles
-        for (int i = 0; i < gateSize("vehicles"); ++i)
-        {
+        for (int i = 0; i < gateSize("vehicles"); ++i) {
             send(contractList->dup(), "vehicles$o", i);
         }
-
-        delete contractList;
     }
 
-    void handleُTaskMetadata(cMessage *msg)
-    {
+    void chooseContract(cMessage *msg) {
+        ContractChoice *choice = check_and_cast<ContractChoice *>(msg);
+        int vehicleId = msg->getArrivalGate()->getIndex();
+        int type = choice->getType();
+        vehicles[vehicleId].sharedResource = contractList->getContracts(type).getResource();
+        vehicles[vehicleId].price = contractList->getContracts(type).getReward();
+    }
+
+    void handleTaskMetadata(cMessage *msg) {
         // cast cMessage to TaskMetadata
+        EV << "Handling task metadata..." << endl;
         TaskMetadata *taskMetadata = check_and_cast<TaskMetadata *>(msg);
 
         int vehicleId = msg->getArrivalGate()->getIndex();
+        EV << "Received task metadata from vehicle: " << vehicleId << endl;
         vehicles[vehicleId].taskResource = taskMetadata->getTaskResource();
         vehicles[vehicleId].taskDataSize = taskMetadata->getTaskDataSize();
         vehicles[vehicleId].delayConstraint = taskMetadata->getDelayConstraint();
         vehicles[vehicleId].isTaskReady = true;
 
-        if (getReadyVehiclesCount() >= taskAssignmentThreshold)
-        {
-            assignTasks();
+        int readyVehiclesCount = getReadyVehiclesCount();
+        EV << "Ready vehicles count: " << readyVehiclesCount << endl;
+        if (readyVehiclesCount >= taskAssignmentThreshold) {
+            EV << "Vehicle vehicles[20] = {" << endl;
+
+            for (int i = 0; i < numVehicles; i++) {
+                Vehicle vehicle = vehicles[i];
+                EV << "    {"
+                          << vehicle.taskResource << ", "
+                          << vehicle.taskDataSize << ", "
+                          << vehicle.delayConstraint << ", "
+                          << vehicle.sharedResource << ", "
+                          << vehicle.price << ", "
+                          << (vehicle.isTaskReady ? "true" : "false");
+                EV << "}" << endl;
+            }
+
+            EV << "};" << endl;
+//            assignTasks();
         }
     }
 
-    int getReadyVehiclesCount()
-    {
+    int getReadyVehiclesCount() {
         int count = 0;
-        for (int i = 0; i < numVehicles; i++)
-        {
-            if (vehicles[i].isTaskReady)
-            {
+        for (int i = 0; i < numVehicles; i++) {
+            if (vehicles[i].isTaskReady) {
                 count++;
             }
         }
         return count;
     }
 
-    void assignTasks()
-    {
+    void assignTasks() {
+        EV << "All vehicles are ready, assigning tasks..." << endl;
         int proposals[numVehicles];
 
         int remainingTasks = 0;
-        for (int i = 0; i < numVehicles; i++)
-        {
+        for (int i = 0; i < numVehicles; i++) {
             proposals[i] = -1;
-            if (vehicles[i].isTaskReady)
-            {
+            vehicles[i].totalTime = new double[numVehicles];
+            if (vehicles[i].isTaskReady) {
                 remainingTasks++;
             }
         }
 
-        while (remainingTasks > 0)
-        {
+        while (remainingTasks > 0) {
             std::vector<int> assignedIds[numVehicles];
-            for (int i = 0; i < numVehicles; i++)
-            {
-                if (proposals[i] != -1)
-                {
+            for (int i = 0; i < numVehicles; i++) {
+                if (proposals[i] != -1) {
                     continue;
                 }
-                int maxPreference = 0;
+                double maxPreference = 0;
                 int maxPreferenceId = i;
-                for (int j = 0; j < numVehicles; j++)
-                {
-                    if (i == j)
-                    {
+                for (int j = 0; j < numVehicles; j++) {
+                    if (i == j) {
                         continue;
                     }
                     vehicles[i].totalTime[j] = vehicles[i].taskResource / vehicles[j].sharedResource;
-                    double transimissionTime = getTransmissionTime(i, j);
-                    if (transimissionTime > getTransimissionConstraint(i, j))
-                    {
+                    double transmissionTime = getTransmissionTime(i, j);
+                    if (transmissionTime > getTransmissionConstraint(i, j)) {
                         continue;
                     }
-                    vehicles[i].totalTime[j] += transimissionTime;
-                    double preference = 1 / vehicles[i].totalTime[j] - vehicles[j].price;
-                    if (preference > maxPreference || maxPreferenceId == i)
-                    {
+                    vehicles[i].totalTime[j] += transmissionTime;
+                    double preference = 1 / vehicles[i].totalTime[j] - vehicles[j].taskPrice;
+                    if (preference > maxPreference || maxPreferenceId == i) {
                         maxPreference = preference;
                         maxPreferenceId = j;
                     }
@@ -233,25 +242,20 @@ protected:
                 assignedIds[maxPreferenceId].push_back(i);
                 remainingTasks--;
             }
-            for (int i = 0; i < numVehicles; i++)
-            {
-                if (assignedIds[i].size() > 1)
-                {
-                    for (int j = 0; j < assignedIds[i].size(); j++)
-                    {
+            for (int i = 0; i < numVehicles; i++) {
+                if (assignedIds[i].size() > 1) {
+                    for (int j = 0; j < assignedIds[i].size(); j++) {
                         int id = assignedIds[i][j];
                         proposals[id] = -1;
                         remainingTasks++;
                     }
-                    vehicles[i].price += getPriceIncrease(i);
+                    vehicles[i].taskPrice += getPriceIncrease(i);
                 }
             }
         }
 
-        for (int i = 0; i < numVehicles; i++)
-        {
-            if (proposals[i] == -1)
-            {
+        for (int i = 0; i < numVehicles; i++) {
+            if (proposals[i] == -1) {
                 continue;
             }
             int nodeId = proposals[i];
@@ -262,18 +266,15 @@ protected:
         }
     }
 
-    double getTransmissionTime(int sourceId, int destinationId)
-    {
+    double getTransmissionTime(int sourceId, int destinationId) {
         return 0;
     }
 
-    double getTransimissionConstraint(int sourceId, int destinationId)
-    {
+    double getTransmissionConstraint(int sourceId, int destinationId) {
         return 1;
     }
 
-    double getPriceIncrease(int id)
-    {
+    double getPriceIncrease(int id) {
         return 1;
     }
 };
--- src/vehicle.cc
+++ src/vehicle.cc
@@ -3,8 +3,7 @@
 
 using namespace omnetpp;
 
-class Vehicle : public cSimpleModule
-{
+class Vehicle : public cSimpleModule {
 private:
     double totalResource;
     double taskDataSize;
@@ -12,8 +11,7 @@ private:
     double delayConstraint;
 
 protected:
-    virtual void initialize() override
-    {
+    virtual void initialize() override {
         // get parameters
         totalResource = par("totalResource");
         taskDataSize = par("taskDataSize");
@@ -21,13 +19,20 @@ protected:
         delayConstraint = par("delayConstraint");
     }
 
-    virtual void handleMessage(cMessage *msg) override
-    {
+    virtual void handleMessage(cMessage *msg) override {
+        if (msg->isSelfMessage()) {
+            // Check if this is the 'prepareContracts' self-message
+            if (strcmp(msg->getName(), "prepareTaskMetadata") == 0) {
+                prepareTaskMetadata();
+            }
+        }
+
         // check if cMessage is of type ContractList
-        if (msg->isName("ContractList"))
-        {
+        if (msg->isName("processContractList")) {
             handleContractList(msg);
         }
+
+        delete msg;
     }
 
     void handleContractList(cMessage *msg) {
@@ -36,20 +41,32 @@ protected:
 
         Contract bestContract = Contract();
         int bestContractIndex = -1;
-        for (int i = 0; i < contractList->getContractsArraySize(); i++)
-        {
+        for (int i = 0; i < contractList->getContractsArraySize(); i++) {
             // get contract from contractList
             Contract contract = contractList->getContracts(i);
 
-            if (contract.getResource() <= totalResource && contract.getReward() >= bestContract.getReward())
-            {
+            if (contract.getResource() <= totalResource && contract.getReward() >= bestContract.getReward()) {
                 bestContract = contract;
                 bestContractIndex = i;
             }
         }
-        ContractChoice *contractChoice = new ContractChoice("ContractChoice");
+        ContractChoice *contractChoice = new ContractChoice("chooseContract");
         contractChoice->setType(bestContractIndex);
+        // print contract choice index, resource, and reward, also with index of vehicle
+        EV_DEBUG << "Contract choice: " << contractChoice->getType() << " from vehicle: " << getIndex() << " with resource: "
+           << bestContract.getResource() << " and reward: " << bestContract.getReward() << endl;
         send(contractChoice, "baseStation$o");
+
+        cMessage *prepTaskMetadataMsg = new cMessage("prepareTaskMetadata");
+        scheduleAt(0, prepTaskMetadataMsg);
+    }
+
+    void prepareTaskMetadata() {
+        TaskMetadata *taskMetadata = new TaskMetadata("handleTaskMetadata");
+        taskMetadata->setTaskResource(taskResource);
+        taskMetadata->setTaskDataSize(taskDataSize);
+        taskMetadata->setDelayConstraint(delayConstraint);
+        send(taskMetadata, "baseStation$o");
     }
 };
 
